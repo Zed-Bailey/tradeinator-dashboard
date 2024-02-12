23f8376d6b81bf6f3ec0d8bb375a4400bd913b4d
Zoran <emailzed03@gmail.com>
2024-02-12 12:24:05 +1100

    .

--- app/Livewire/StrategyListTable.php
+++ app/Livewire/StrategyListTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\SavedStrategy;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Notifications\Notification;
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full max-w-7xl grid grid-cols-12">
+<div class="w-full max-w-7xl grid grid-cols-12 space-x-2 space-y-2">
     <div class="col-span-6">
 test
     </div>
@@ -8,6 +8,10 @@ test
 
     <div class="col-span-12">
         @livewire('strategy-list-table')
+    </div>
+
+    <div class="col-span-12">
+        @livewire(App\Livewire\InstrumentPriceChart::class)
     </div>
 
 </div>
