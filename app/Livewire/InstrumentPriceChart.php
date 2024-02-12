06c9e547cb92f9dbd90c44a9e55a4e6c1218dbbb
Zoran <emailzed03@gmail.com>
2024-02-12 14:42:50 +1100

    Create InstrumentPriceChart.php

---
+++ app/Livewire/InstrumentPriceChart.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Helpers\OandaApi;
+use App\Models\Enums\CandlestickGranularity;
+use App\Models\Enums\InstrumentNames;
+use Livewire\Component;
+use Livewire\Livewire;
+
+class InstrumentPriceChart extends Component
+{
+    public string $selectedInstrument;
+    public string $selectedGranularity;
+
+
+    protected OandaApi $api;
+
+    public function mount(OandaApi $api) {
+        $this->api = $api;
+        $this->selectedInstrument = "AUD_USD";
+        $this->selectedGranularity = "M30";
+    }
+
+
+    public function getData() {
+        $this->dispatch('updateChartData')->to(InstrumentPriceChartWidget::class);
+//        error_log($this->selectedInstrument);
+//        error_log($this->selectedGranularity);
+
+    }
+
+    public function render()
+    {
+        return view('livewire.instrument-price-chart');
+    }
+}
