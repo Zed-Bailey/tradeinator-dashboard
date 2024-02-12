b98407af419fdec8160bbb12c0a77095666aae24
Zoran <emailzed03@gmail.com>
2024-02-12 11:45:00 +1100

    add enums for candlestick instruments and granularity

---
+++ app/Models/Enums/CandlestickGranularity.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Enums;
+
+enum CandlestickGranularity {
+    case S5;
+    case S10;
+    case S15;
+    case S30;
+    case M1;
+    case M2;
+    case M4;
+    case M5;
+    case M10;
+    case M15;
+    case M30;
+    case H1;
+    case H2;
+    case H3;
+    case H4;
+    case H6;
+    case H8;
+    case H12;
+    case D;
+    case W;
+    case M;
+}
